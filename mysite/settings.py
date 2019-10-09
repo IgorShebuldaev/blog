4e545a1a3b1f4a5c5deeb91999b1962195f7eb54
Igor Shebuldaev <igor.shebuldaev@gmail.com>
2019-10-09 13:32:08 +0300

    update design and change debug mode on true

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a#vy63vksj*1o&88x%r6!(x0(&xj61*!ohigsz+5kq8$9wl0r*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
